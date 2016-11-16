cc64ff75b6de045b7fcaa0a52ed10d02b6c178f4
JaredGordon <jgordon@pivotal.io>
2016-11-16 14:28:32 -0500

    changed pipeline to refect the tile-history.yml with versioning

--- kafka-broker/ci/build-tile/task.sh
+++ kafka-broker/ci/build-tile/task.sh
@@ -4,12 +4,13 @@ cd tile-repo/kafka-broker
 
 mkdir target
 cp ../../broker-jar/*.jar target/kafka-broker.jar
-tile build
+
+ver=`more ../../version/number`
+tile build ${ver}
 
 file=`ls product/*.pivotal`
 filename=$(basename "${file}")
 filename="${filename%-*}"
-ver=`more ../../version/number`
 
 cp ${file} ../../broker-tile/${filename}-${ver}.pivotal
-cp tile-history.yml ../../tile-history-new/tile-history.yml
+cp tile-history.yml ../../tile-history-new/tile-history-${ver}.yml

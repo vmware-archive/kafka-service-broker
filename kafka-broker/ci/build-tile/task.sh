8d7cdf0db4ac45c5f9dd45318cdd3a9d913273e0
JaredGordon <jgordon@pivotal.io>
2016-11-16 12:41:38 -0500

    reuse same tile-hisotry file.

--- kafka-broker/ci/build-tile/task.sh
+++ kafka-broker/ci/build-tile/task.sh
@@ -12,4 +12,4 @@ filename="${filename%-*}"
 ver=`more ../../version/number`
 
 cp ${file} ../../broker-tile/${filename}-${ver}.pivotal
-cp tile-history*.yml ../../tile-history-new/tile-history-${ver}.yml
+cp tile-history.yml ../../tile-history-new/tile-history.yml

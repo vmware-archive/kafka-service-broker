abedb9738cb70a936e5bcb01bac2f293125458b8
JaredGordon <jgordon@pivotal.io>
2017-03-13 14:13:48 -0400

    tested against gcp pcf.

--- kafka-sample-producer/src/main/java/io/pivotal/cf/service/producer/app/KafkaSampleProducer.java
+++ kafka-sample-producer/src/main/java/io/pivotal/cf/service/producer/app/KafkaSampleProducer.java
@@ -1,72 +1,49 @@
 /**
- Copyright (C) 2016-Present Pivotal Software, Inc. All rights reserved.
-
- This program and the accompanying materials are made available under
- the terms of the under the Apache License, Version 2.0 (the "License”);
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright (C) 2016-Present Pivotal Software, Inc. All rights reserved.
+ * <p>
+ * This program and the accompanying materials are made available under
+ * the terms of the under the Apache License, Version 2.0 (the "License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.pivotal.cf.service.producer.app;
 
 import io.pivotal.cf.service.connector.KafkaRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.kafka.support.SendResult;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Slf4j
 @Component
-public class KafkaSampleProducer implements ApplicationListener<ContextRefreshedEvent> {
+public class KafkaSampleProducer {
 
     private KafkaRepository repo;
 
-    public KafkaSampleProducer(KafkaRepository repo) {
+    public KafkaSampleProducer(KafkaRepository repo) throws Exception {
         this.repo = repo;
     }
 
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    private void produceMessages() throws ExecutionException, InterruptedException {
-
-        while (true) {
-            ListenableFuture<SendResult<Integer, String>> future = repo.sendMessage("hello from the KafkaSampleProducer: " + System.currentTimeMillis());
-            try {
-                SendResult<Integer, String> result = future.get();
-                log.info( result.getProducerRecord().value() + " ....Message .... was sent to Topic " + result.getRecordMetadata().topic());
-                TimeUnit.SECONDS.sleep(10);
-            } catch (Exception e) {
-                log.warn("KafkaSampleProducer interrupted.", e);
-                throw e;
-            }
+    @Scheduled(fixedRate = 10000)
+    public void produceMessage() {
+        log.info("sending message: the time is now {}", dateFormat.format(new Date()));
+        try {
+            repo.sendMessage("hello from the KafkaSampleProducer: " + System.currentTimeMillis());
+        } catch (Exception e) {
+            log.error("KafkaSampleProducer interrupted.", e);
         }
-    }
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-
-        (new Thread() {
-            public void run() {
-                try {
-                    produceMessages();
-                } catch (Exception e) {
-                    log.error("Producer crapped out.", e);
-                }
-            }
-        }).start();
-
     }
 }
